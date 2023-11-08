ad3f92056d5528a3b644d724c92c3e6dd13558bb
Dima Spodeniuk <dima.spodeniuk@gmail.com>
2023-11-08 10:19:46 +0200

    feat: project packages setup

---
+++ fantasticonrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    inputDir: './src/assets/font-icons',
+    outputDir: './public/fonts/icon',
+    fontsUrl: '/fonts/icon',
+    assetTypes: ['ts', 'scss'],
+    pathOptions: {
+        scss: './src/assets/scss/_icons.scss',
+        ts: './src/shared/types/icons.ts',
+    },
+};
