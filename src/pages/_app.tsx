f4795be1c8ddbd9b6b5e6826451a31a2e87cd15a
Dima Spodeniuk <dima.spodeniuk@gmail.com>
2023-11-08 10:21:59 +0200

    feat(_app.tsx): use appWithTranslation & styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { appWithTranslation } from 'next-i18next';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import 'react-toastify/dist/ReactToastify.css';
+import 'rc-slider/assets/index.css';
+import '../styles.scss';
+
+const Root = ({ Component, pageProps }: AppProps) => {
+    return <Component {...pageProps} />;
+};
+
+export default appWithTranslation(Root);
